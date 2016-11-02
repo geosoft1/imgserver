85a211065130d1485dee41c1c4a4e3dd57d5cfbd
geosoft1 <geosoft1@gmail.com>
2016-11-02 09:23:52 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,56 @@
+// imgserver project
+// Copyright (C) 2016  geosoft1  geosoft1@gmail.com
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	//CORS response (http://www.html5rocks.com/en/tutorials/cors/)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-type", "image/jpg")
+
+	now := time.Now()
+
+	filename := "img/" + now.Format("0201") + ".jpg"
+	log.Println(filename)
+
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		filename = "Untitled.jpg"
+	}
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.Write(file)
+}
+
+func main() {
+	// Discourse Admin:Customize:CSS/HTML:Top
+	// <!--img src="http://192.168.88.223:8080/" height=150px></img-->
+	// Mobile:Top
+	// <!--img src="http://192.168.88.223:8080/" width=100%</img-->
+
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
+}
